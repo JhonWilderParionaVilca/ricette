07d71d3120029257bafac870860d6b63aa48f1f6
wilderPariona <jhon.pariona.27@unsch.edu.pe>
2020-06-22 22:55:19 +0000

    Deploying to gh-pages from master @ 5320426f721e8b4a1bb149824cda246172e48d1c 🚀

---
+++ precache-manifest.c76ff64eae106ae573324c532db5cb03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0ed462db015109ce1396e60125ef36",
+    "url": "/ricette/index.html"
+  },
+  {
+    "revision": "3cb635b1238f5ed6abcc",
+    "url": "/ricette/static/css/main.d59bfdbb.chunk.css"
+  },
+  {
+    "revision": "75911a69fce9c194c87c",
+    "url": "/ricette/static/js/2.0d0dab9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ricette/static/js/2.0d0dab9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cb635b1238f5ed6abcc",
+    "url": "/ricette/static/js/main.f56216d7.chunk.js"
+  },
+  {
+    "revision": "161fadcd88b7d11b03d8",
+    "url": "/ricette/static/js/runtime-main.3794f476.js"
+  }
+]);
